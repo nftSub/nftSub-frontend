f60bf9a4915b77fdc0cff278a23db86f3dde8d68
Blockchain Oracle <abubakrjimoh16488@gmail.com>
2025-09-25 19:42:45 +0100

    Add SVG logo and update Header component to use PNG version
    - Introduced a new SVG logo file for nftSub.
    - Updated Header component to reference the new PNG logo instead of the SVG.
    - Ensured consistent branding by replacing all instances of the SVG logo with the PNG in the Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Image
-              src="/nftSub.svg"
+              src="/images/nftSub.png"
               alt="nftSub Logo"
               width={32}
               height={32}
@@ -69,7 +69,7 @@ export default function Header() {
             <nav className="flex md:hidden">
               <Link href="/" className="flex items-center space-x-2">
                 <Image
-                  src="/nftSub.svg"
+                  src="/images/nftSub.png"
                   alt="nftSub Logo"
                   width={32}
                   height={32}
